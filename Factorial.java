5135895cdb755477e8f339a2126ad15b8b06e9ed
Sairmayee <64839434+Sairmayee@users.noreply.github.com>
2022-03-23 15:12:27 +0530

    Add files via upload
    Updated as per instruction in the problem statement. Thank you.

--- Factorial.java
+++ Factorial.java
@@ -2,11 +2,18 @@ package week1.day1;
 
 public class Factorial {
 	public static void main(String[] args) {
+		//		int fact = 1;
+		//		int value = 5;
+		//		for (int input = 1; input <= value; input++) {
+		//			fact = fact*input;
+		//		}
+		//	System.out.println(fact);
+
+		int input;
 		int fact = 1;
-		int value = 5;
-		for (int input = 1; input <= value; input++) {
-			fact = fact*input;
+		for (input = 5 ; input > 0 ; input-- ) {
+			fact = fact * input;
 		}
-	System.out.println(fact);
-}
+		System.out.println("Factorial is " + fact);
+	}
 }
