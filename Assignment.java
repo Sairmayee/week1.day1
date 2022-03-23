f1996d3a5427815fa20b0c3cecf3e191809f83fa
Sairmayee <64839434+Sairmayee@users.noreply.github.com>
2022-03-23 15:03:05 +0530

    Add files via upload
    Updated -ve to +ve number code

--- Assignment.java
+++ Assignment.java
@@ -2,9 +2,9 @@ package week1.day1;
 
 public class Assignment {
 	public static void main(String[] args) {
-
-		for (int number = -40; number < 0; number = 40)
-		{
+		int number = -40;
+		if (number < 0) {
+			number = 40;
 			System.out.println("The number -40 is converted to 40");
 
 		}
@@ -12,12 +12,10 @@ public class Assignment {
 		int number2 = 35;
 		if (number2 > 0) {
 			System.out.println("The number is positive");
-	
-		}
-		else if (number2 < 0) {
+
+		} else if (number2 < 0) {
 			System.out.println("The number is negative");
-		}
-		else
+		} else
 			System.out.println("The number is neither positive nor negative");
 
 	}
